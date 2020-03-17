fe32d59f5309f271cfd1bbcce982bcb76de3da7e
Adith@LAPTOP-8P0SMJ2T <adith442864@gmail.com>
2020-03-17 20:14:01 +0530

    added driver to deals page constructor

--- src/main/java/com/qa/hubspot/page/DealsPage.java
+++ src/main/java/com/qa/hubspot/page/DealsPage.java
@@ -1,11 +1,17 @@
 package com.qa.hubspot.page;
 
+import org.openqa.selenium.WebDriver;
+
 import com.qa.hubspot.base.BasePage;
 
 public class DealsPage extends BasePage {
 	
-	public DealsPage() {
+	public WebDriver driver;
+	
+	public DealsPage(WebDriver driver) {
+		this.driver=driver;
 		System.out.println("deals page constructor...");
+		
 	}
 
 }
